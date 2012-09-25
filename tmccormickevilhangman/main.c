11b2413ad824f31c583a76f6c509a0f06c926227
Tristan J. McCormick <tjmccorm@searles128-05.campus.bowdoin.edu>
2012-09-25 15:50:44 -0400

    Initial Commit

---
+++ tmccormickevilhangman/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tmccormickevilhangman
+//
+//  Created by Tristan J. McCormick on 2012-09-25.
+//  Copyright (c) 2012 Tristan J. McCormick. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
